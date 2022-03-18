fd94ad2ae9820370639be227d3643ad0cfdc57b3
unknown <crazymichuzz@gmail.com>
2022-03-18 06:11:13 -0800

    Updates

---
+++ precache-manifest.05e1c9cd5568330c60d256894ff82d8f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeed411313f055a9f959a7f9262da308",
+    "url": "/datacamp/index.html"
+  },
+  {
+    "revision": "685b408398719e3ef7ec",
+    "url": "/datacamp/static/css/main.65a832fd.chunk.css"
+  },
+  {
+    "revision": "d1e8de39a6d1bd195deb",
+    "url": "/datacamp/static/js/2.2d016ddc.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/datacamp/static/js/2.2d016ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685b408398719e3ef7ec",
+    "url": "/datacamp/static/js/main.82b38b33.chunk.js"
+  },
+  {
+    "revision": "cc8e6d20dfb2b84cba57",
+    "url": "/datacamp/static/js/runtime-main.5688cc9a.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/datacamp/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/datacamp/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/datacamp/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/datacamp/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/datacamp/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/datacamp/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/datacamp/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/datacamp/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/datacamp/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/datacamp/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/datacamp/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/datacamp/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/datacamp/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/datacamp/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
